aac2d1427053a8f7c67f82fb2b851e3462ddd10c
Shvd <2100992@gmail.com>
2019-11-05 09:31:31 +0300

    added some features

--- my_site/p_library/models.py
+++ my_site/p_library/models.py
@@ -6,6 +6,9 @@ class Author(models.Model):
     birth_year = models.SmallIntegerField()
     country = models.CharField(max_length=2)
 
+    def __str__(self):
+        return self.full_name
+
 class Book(models.Model):
     ISBN = models.CharField(max_length=13)
     title = models.TextField()
@@ -14,3 +17,6 @@ class Book(models.Model):
     copy_count = models.SmallIntegerField(default=1)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     author = models.ForeignKey(Author, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- my_site/p_library/views.py
+++ my_site/p_library/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from p_library.models import Book, Author
+from django.template import loader
 
 # Create your views here.
+
+def books_list(request):
+    books = Book.objects.all()
+    return HttpResponse(books)
+
+def index(request):
+    template = loader.get_template('index.html')
+    # books_count = Book.objects.all().count()
+    books = Book.objects.all()
+    biblio_data = {
+        "title": 'мою библиотеку',
+        'books': books,
+    }
+    return HttpResponse(template.render(biblio_data))
